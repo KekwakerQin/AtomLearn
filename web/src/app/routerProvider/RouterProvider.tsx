5c2205f8816323cbe9422b89642a3e0b82be2109
emptygxd <arsenii_rusin@mail.ru>
2025-12-13 18:37:07 +0300

    router config

--- web/src/app/routerProvider/RouterProvider.tsx
+++ web/src/app/routerProvider/RouterProvider.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { App } from "../App";
-import { LoginPage, RegisterPage, ProfilePage } from "@features";
+import { LoginPage, RegisterPage } from "@pages";
+import { Layout } from "@app/layout/Layout";
 
 export const RouterProvider = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
+        <Route path="/" element={<Layout />}>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
-          <Route path="profile" element={<ProfilePage />} />
+          {/* <Route path="profile" element={<ProfilePage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
--- web/src/features/auth/ui/registerForm/RegisterForm.tsx
+++ web/src/features/auth/ui/registerForm/RegisterForm.tsx
@@ -1,3 +1,38 @@
+import { registerUser } from "@features";
+import { useState } from "react";
+
 export const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | undefined>("");
+
+  async function handleRegister(e: React.FormEvent) {
+    e.preventDefault();
+    const res = await registerUser(email, password);
+
+    if (!res.ok) {
+      setError(res.error);
+    } else {
+      console.log("Регистрация прошла успешно:", res.user);
+    }
+  }
+  return (
+    <form onSubmit={handleRegister}>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Пароль"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Зарегистрироваться</button>
+
+      {error && <div style={{ color: "red" }}>{error}</div>}
+    </form>
+  );
 };
